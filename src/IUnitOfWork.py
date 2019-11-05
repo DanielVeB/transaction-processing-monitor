96f591ad6d6126f3d80370fbed7daf0680e9ab80
GorobetsAndrii <andrey.gorobets99@gmail.com>
2019-11-05 23:25:03 +0100

    update IUnitOfWork

--- src/IUnitOfWork.py
+++ src/IUnitOfWork.py
@@ -4,8 +4,18 @@ from abc import abstractmethod
 class IUnitOfWork:
     @abstractmethod
     def registerNew(self): raise NotImplementedError
+
+    @abstractmethod
     def registerDirty(self): raise NotImplementedError
+
+    @abstractmethod
     def registerClean(self): raise NotImplementedError
+
+    @abstractmethod
     def registerDelete(self): raise NotImplementedError
+
+    @abstractmethod
     def commit(self): raise NotImplementedError
+
+    @abstractmethod
     def rollback(self): raise NotImplementedError
