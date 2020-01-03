1df8789e17dcc86c621b5f93f21665f6bcc1c6a1
DanielVeB <danielkurosz@gmail.com>
2020-01-03 22:57:46 +0100

    Add serialization

--- src/dto/resource.py
+++ src/dto/resource.py
@@ -1,12 +1,18 @@
 from uuid import uuid1
 
 
-class DpResource:
+class DpResource(object):
 
     def __init__(self, host, port, user, password):
-        self.id = uuid1()
         self.host = host
         self.port = port
         self.user = user
         self.password = password
 
+    def serialize(self):
+        return {
+            "host": self.host,
+            "port": self.port,
+            "user": self.user,
+            "password": self.password
+        }
