da41e9625f4f39bab9803303495b9cf0f0487aad
GorobetsAndrii <43909562+GorobetsAndrii@users.noreply.github.com>
2020-01-07 21:36:35 +0100

    Add files via upload

---
+++ src/logic/json_to_object.py
@@ -0,0 +1,11 @@
+import json
+from collections import namedtuple
+
+def json2obj(data):
+    return json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+"""
+data = '{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
+x = json2obj(data)
+print(x.name)
+"""
