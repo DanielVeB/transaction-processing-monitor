3726b8c0291467777cbd66a8a11e8898f8706b91
GorobetsAndrii <andrey.gorobets99@gmail.com>
2020-01-23 01:38:16 +0100

    Merge branch 'master' of C:\Users\Andriy Gorobets\PycharmProjects\transaction-processing-monitor with conflicts.

--- src/entity/request.py
+++ src/entity/request.py
@@ -3,55 +3,57 @@ from uuid import UUID
 
 
 @dataclass
-class DP_Repository:
-    host: str
-    port: str
-    endpoints: []
-
-
-@dataclass
 class DP_Transaction:
-    id: UUID
-    statements: []
+    id : UUID
+    statements : []
 
     def toString(self):
         return "Database with id" + str(self.id) + "statements: " + str(self.statements)
 
-
 @dataclass
 class DP_Statement:
     # INSERT, UPDATE or DELETE
-    method: str
-    table_name: str
+    method : str
+    table_name : str
     # field_name : value
-    values: {}
+    values : {}
     # SQL statement
-    where: str = None
+    where : str = None
 
     def toSQL(self):
         result = ""
+        select = ""
 
         if self.method == "INSERT":
             keys = ','.join(self.values.keys())
             values = list(self.values.values())
-            table = self.table_name + "(" + keys + ")"
-            for i in range(0, len(values)):
-                if isinstance(values[i], int):
+            result = self.method + " INTO " + self.table_name + "(" + keys + ")" + " VALUES "
+            for i in range(0,len(values)):
+                if isinstance(values[i],int):
                     result += str(values[i]) + ","
                 else:
                     result += "'" + values[i] + "',"
             result = result[:-1]
-            return table, result
+            if self.where != None:
+                result += " WHERE " + self.where
 
         elif self.method == "UPDATE":
             keys = list(self.values.keys())
             values = list(self.values.values())
+            result = self.method + " " + self.table_name + " SET "
             for i in range(0, len(values)):
                 if isinstance(values[i], int):
                     result += keys[i] + " = " + str(values[i]) + ","
                 else:
                     result += keys[i] + " = '" + values[i] + "',"
             result = result[:-1]
-            return self.table_name, result, self.where
+            if self.where != None:
+                result += " WHERE " + self.where
+            keys = ','.join(self.values.keys())
+            select = "SELECT " + keys + " FROM " + self.table_name
         else:
-            return self.table_name, self.where
+            keys = ','.join(self.values.keys())
+            result = "DELETE FROM " + self.table_name + " WHERE " + self.where
+            select = "SELECT " + keys +  " FROM " + self.table_name
+
+        return result,select
