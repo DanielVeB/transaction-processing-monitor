4849502c9cd831c23a1f57b4b7f205e8ce306a3d
Liptonski <mcieslik@student.agh.edu.pl>
2020-01-24 14:46:51 +0100

    fix of not working database

--- src/library/web_service.py
+++ src/library/web_service.py
@@ -10,42 +10,47 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
+app = Flask(__name__)
+
 
 class WebService(IWebService):
     logger = logging.getLogger(__name__)
-    app = Flask(__name__)
 
-    def __init__(self, db_name, username, pwd, db_type="mysql", db_uri="localhost", port=":3306"):
-        self.uri = db_type + "://" + username + ":" + pwd + "@" + db_uri + port + "/" + db_name
-        self.repository = self.create_repository(self.uri)
+    def __init__(self, uri=None, db_name=None, username=None, pwd=None, db_type="mysql", db_uri="localhost", port=":3306"):
+        self.uri = uri if uri is not None else db_type + "://"\
+                                               + username + ":" + pwd + "@" \
+                                               + db_uri + port + "/" + db_name
 
-    def create_repository(self, connection_uri):
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = connection_uri
-        database_connection = SQLAlchemy(self.app)
-        return Repository(database_connection, self.app)
+    def create_repository(self, db_app):
+        app.config['SQLALCHEMY_DATABASE_URI'] = self.uri
+        database_connection = SQLAlchemy(db_app)
+        return Repository(database_connection, db_app)
 
-    # Transaction request
-    @app.route('/database/transaction', methods=['POST'])
-    def execute_transaction(self):
-        transaction = request.get_json()
-        result = self.repository.execute_statement(transaction)
-        return jsonify(result)
+uri = "postgres://gmavcrpg:k0fO5TZwnQrXFh7e8Q2lcJOZYjAgaT3g@rajje.db.elephantsql.com:5432/gmavcrpg"
+webservice = WebService(uri)
+repository = webservice.create_repository(app)
 
-    # Should this methods have jsonify return?
-    # Commit
-    @app.route('/database/commmit', methods=['POST'])
-    def commit(self):
-        result = self.repository.commit()
-        return result
+# Transaction request
+@app.route('/database/transaction', methods=['POST'])
+def execute_transaction():
+    transaction = request.get_json()
+    result = repository.execute_statement(transaction)
+    return jsonify(result)
 
-    # Rollback
-    @app.route('/database/rollback', methods=['POST'])
-    def rollback(self):
-        result = self.repository.rollback()
-        return result
+# Should this methods have jsonify return?
+# Commit
+@app.route('/database/commmit', methods=['POST'])
+def commit():
+    result = repository.commit()
+    return result
 
-    def run_web_service(self, port):
-        self.app.run(host='0.0.0.0', port=port)
+# Rollback
+@app.route('/database/rollback', methods=['POST'])
+def rollback():
+    result = repository.rollback()
+    return result
 
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
 
--- src/service_one.py
+++ src/service_one.py
@@ -1,7 +1,3 @@
 from src.library.web_service import WebService
 
 
-webservice = WebService("test", "root", "lukasz")
-
-if __name__ == '__main__':
-    webservice.run_web_service(8081)
